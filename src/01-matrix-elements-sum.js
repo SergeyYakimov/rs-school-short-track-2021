bd84ea661414f4ca6fd2d15ccc05a3aa854fd014
SergeyYakimov <teriertis@yandex.ru>
2021-04-08 14:58:18 +0800

    feat: make number 1 task

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,20 +14,19 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  // let sum = 0;
-  // let flag = true;
-  // for (let i = 0; i < matrix[0].length; i++) {
-  //   flag = true;
-  //   for (let j = 0; j < matrix.length && flag; j++) {
-  //     if (!matrix[i][j] || matrix[i][j] === 0) {
-  //       flag = false;
-  //     } else {
-  //       flag = true;
-  //       sum += matrix[i][j];
-  //     }
-  //   }
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[j][i] === 0) {
+        break;
+      } else {
+        sum += matrix[j][i];
+      }
+    }
+  }
+
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
