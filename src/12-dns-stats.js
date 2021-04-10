302c1e6a8e753c674f2c09674eb3fa65da20b262
SergeyYakimov <teriertis@yandex.ru>
2021-04-10 17:26:24 +0800

    feat: make number 12 task

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,34 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const arr = [];
+  const obj = {};
+  for (let i = 0; i < domains.length; i++) {
+    arr.push(domains[i].split('.').reverse());
+  }
+
+  for (let j = 0; j < arr.length; j++) {
+    for (let k = 0; k < arr[j].length; k++) {
+      if (k === 0) {
+        arr[j][k] = `.${arr[j][k]}`;
+      } else {
+        arr[j][k] = `${arr[j][k - 1]}.${arr[j][k]}`;
+      }
+    }
+  }
+
+  const domainsArr = arr.flat(1);
+
+  for (let m = 0; m < domainsArr.length; m++) {
+    if (typeof obj[domainsArr[m]] === 'undefined') {
+      obj[domainsArr[m]] = 1;
+    } else {
+      obj[domainsArr[m]] += 1;
+    }
+  }
+
+  return obj;
 }
 
 module.exports = getDNSStats;
